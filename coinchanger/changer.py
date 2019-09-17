dcc612abe05d7026e0889c370baf30f7081382d3
Sam Jones <sam.jones494@googlemail.com>
2019-09-17 21:54:00 +0100

    remove mutable default argument and refactor calculate method tests

--- coinchanger/changer.py
+++ coinchanger/changer.py
@@ -2,7 +2,8 @@ class CoinChanger:
     def __init__(self, denominations=(200, 100, 50, 20, 10, 5, 2, 1)):
         self.denominations = denominations
 
-    def calculate(self, change_amount, coins=[]):
+    def calculate(self, change_amount, coins=None):
+        if coins == None: coins = []
         for coin in self.denominations:
             if coin <= change_amount:
                 change_amount -= coin
--- tests/test_changer.py
+++ tests/test_changer.py
@@ -8,48 +8,48 @@ def coin_changer():
 
 
 def test_passing_zero_returns_an_empty_array(coin_changer):
-    assert coin_changer.calculate(0, []) == []
+    assert coin_changer.calculate(0) == []
 
 
 def test_passing_one_returns_a_single_penny(coin_changer):
-    assert coin_changer.calculate(1, []) == [1]
+    assert coin_changer.calculate(1) == [1]
 
 
 def test_passing_two_returns_a_single_2p_coin(coin_changer):
-    assert coin_changer.calculate(2, []) == [2]
+    assert coin_changer.calculate(2) == [2]
 
 
 def test_passing_five_returns_a_single_5p_coin(coin_changer):
-    assert coin_changer.calculate(5, []) == [5]
+    assert coin_changer.calculate(5) == [5]
 
 
 def test_passing_10_returns_a_single_10p_coin(coin_changer):
-    assert coin_changer.calculate(10, []) == [10]
+    assert coin_changer.calculate(10) == [10]
 
 
 def test_passing_50_returns_a_single_50p_coin(coin_changer):
-    assert coin_changer.calculate(50, []) == [50]
+    assert coin_changer.calculate(50) == [50]
 
 
 def test_passing_100_returns_a_single_1_pound_coin(coin_changer):
-    assert coin_changer.calculate(100, []) == [100]
+    assert coin_changer.calculate(100) == [100]
 
 
 def test_passing_200_returns_a_single_2_pound_coin(coin_changer):
-    assert coin_changer.calculate(200, []) == [200]
+    assert coin_changer.calculate(200) == [200]
 
 
 def test_returns_two_coins_where_necessary(coin_changer):
-    assert coin_changer.calculate(3, []) == [2, 1]
-    assert coin_changer.calculate(11, []) == [10, 1]
+    assert coin_changer.calculate(3) == [2, 1]
+    assert coin_changer.calculate(11) == [10, 1]
 
 
 def test_returns_three_coins_where_necessary(coin_changer):
-    assert coin_changer.calculate(16, []) == [10, 5, 1]
-    assert coin_changer.calculate(23, []) == [20, 2, 1]
+    assert coin_changer.calculate(16) == [10, 5, 1]
+    assert coin_changer.calculate(23) == [20, 2, 1]
 
 
 def test_returns_multiple_coins_for_complex_cases(coin_changer):
-    assert coin_changer.calculate(227, []) == [200, 20, 5, 2]
-    assert coin_changer.calculate(77, []) == [50, 20, 5, 2]
-    assert coin_changer.calculate(373, []) == [200, 100, 50, 20, 2, 1]
+    assert coin_changer.calculate(227) == [200, 20, 5, 2]
+    assert coin_changer.calculate(77) == [50, 20, 5, 2]
+    assert coin_changer.calculate(373) == [200, 100, 50, 20, 2, 1]
