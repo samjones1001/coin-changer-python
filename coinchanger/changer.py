00df024433771de8e9b00581bf380b5f3b9c627a
Sam Jones <sam.jones494@googlemail.com>
2019-09-17 10:24:45 +0100

    passing 1 to calculate function returns a single penny

--- coinchanger/changer.py
+++ coinchanger/changer.py
@@ -1,3 +1,4 @@
 class CoinChanger:
     def calculate(self, change_amount):
+        if change_amount == 1: return [1]
         return []
--- tests/test_changer.py
+++ tests/test_changer.py
@@ -4,3 +4,7 @@ from context import changer
 def test_passing_nothing_returns_an_empty_array():
     coinchanger = changer.CoinChanger()
     assert coinchanger.calculate(0) == []
+
+def test_passing_one_returns_a_single_penny():
+    coinchanger = changer.CoinChanger()
+    assert coinchanger.calculate(1) == [1]
