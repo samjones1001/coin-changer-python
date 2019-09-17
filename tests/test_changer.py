e55786ef633884d1816cb35aba10e13ab48c7398
Sam Jones <sam.jones494@googlemail.com>
2019-09-17 10:38:45 +0100

    refactor tests for repetition

--- tests/test_changer.py
+++ tests/test_changer.py
@@ -1,18 +1,23 @@
+import pytest
 from context import changer
 
 
-def test_passing_zero_returns_an_empty_array():
-    coinchanger = changer.CoinChanger()
-    assert coinchanger.calculate(0) == []
+@pytest.fixture(scope="function", autouse=True)
+def coinChanger():
+    return changer.CoinChanger()
 
-def test_passing_one_returns_a_single_penny():
-    coinchanger = changer.CoinChanger()
-    assert coinchanger.calculate(1) == [1]
 
-def test_passing_two_returns_a_single_2p_coin():
-    coinchanger = changer.CoinChanger()
-    assert coinchanger.calculate(2) == [2]
+def test_passing_zero_returns_an_empty_array(coinChanger):
+    assert coinChanger.calculate(0) == []
 
-def test_passing_five_returns_a_single_5p_coin():
-    coinchanger = changer.CoinChanger()
-    assert coinchanger.calculate(5) == [5]
+
+def test_passing_one_returns_a_single_penny(coinChanger):
+    assert coinChanger.calculate(1) == [1]
+
+
+def test_passing_two_returns_a_single_2p_coin(coinChanger):
+    assert coinChanger.calculate(2) == [2]
+
+
+def test_passing_five_returns_a_single_5p_coin(coinChanger):
+    assert coinChanger.calculate(5) == [5]
