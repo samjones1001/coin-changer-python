cf7cdbccff7810400289bab73428914afbee34c3
Sam Jones <sam.jones494@googlemail.com>
2019-09-17 10:27:28 +0100

    passing 5 to calculate function returns single 5p coin

--- coinchanger/changer.py
+++ coinchanger/changer.py
@@ -1,4 +1,5 @@
 class CoinChanger:
     def calculate(self, change_amount):
         if change_amount == 1: return [1]
+        if change_amount == 2: return [2]
         return []
--- tests/test_changer.py
+++ tests/test_changer.py
@@ -1,10 +1,14 @@
 from context import changer
 
 
-def test_passing_nothing_returns_an_empty_array():
+def test_passing_zero_returns_an_empty_array():
     coinchanger = changer.CoinChanger()
     assert coinchanger.calculate(0) == []
 
 def test_passing_one_returns_a_single_penny():
     coinchanger = changer.CoinChanger()
     assert coinchanger.calculate(1) == [1]
+
+def test_passing_two_returns_a_single_2p_coin():
+    coinchanger = changer.CoinChanger()
+    assert coinchanger.calculate(2) == [2]
