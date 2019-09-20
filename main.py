29cd994fda4baea7c54ea46d0abee7efb5830e03
Sam Jones <sam.jones494@googlemail.com>
2019-09-20 11:22:23 +0100

    fix broken tests

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ def main():
     if len(sys.argv) < 2:
         print("Please pass an argument")
     else:
-        coin_changer = changer.CoinChanger()
-        print(coin_changer.calculate(int(sys.argv[1])))
+        print(changer.calculate(int(sys.argv[1])))
 
 
 if __name__ == '__main__': main()
