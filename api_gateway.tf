594576a15f644ee61d95b977fbe670c198ad35ce
Sam Jones <sam.jones494@googlemail.com>
2019-09-23 15:39:53 +0100

    clean up terraform config files

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,27 +1,27 @@
 resource "aws_api_gateway_rest_api" "coin-changer-api" {
-    name = "CoinChanger"
+    name =        "CoinChanger"
     description = "Coin Changer API"
 }
 
 resource "aws_api_gateway_resource" "changer" {
-    rest_api_id = "${aws_api_gateway_rest_api.coin-changer-api.id}"
-    parent_id = "${aws_api_gateway_rest_api.coin-changer-api.root_resource_id}"
-    path_part = "changer"
+    rest_api_id = aws_api_gateway_rest_api.coin-changer-api.id
+    parent_id =   aws_api_gateway_rest_api.coin-changer-api.root_resource_id
+    path_part =   "changer"
 }
 
 resource "aws_api_gateway_method" "changer" {
-    rest_api_id = "${aws_api_gateway_rest_api.coin-changer-api.id}"
-    resource_id = "${aws_api_gateway_resource.changer.id}"
+    rest_api_id = aws_api_gateway_rest_api.coin-changer-api.id
+    resource_id = aws_api_gateway_resource.changer.id
     http_method = "POST"
     authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "changer" {
-    rest_api_id = "${aws_api_gateway_rest_api.coin-changer-api.id}"
-    resource_id = "${aws_api_gateway_resource.changer.id}"
-    http_method = "${aws_api_gateway_method.changer.http_method}"
-    type = "AWS_PROXY"
-    uri = "${aws_lambda_function.coin_changer.invoke_arn}"
+    rest_api_id =             aws_api_gateway_rest_api.coin-changer-api.id
+    resource_id =             aws_api_gateway_resource.changer.id
+    http_method =             aws_api_gateway_method.changer.http_method
+    type =                    "AWS_PROXY"
+    uri =                     aws_lambda_function.coin_changer.invoke_arn
     integration_http_method = "POST"
 }
 
@@ -30,10 +30,10 @@ resource "aws_api_gateway_deployment" "coin-changer-api" {
         "aws_api_gateway_integration.changer"
     ]
 
-    rest_api_id = "${aws_api_gateway_rest_api.coin-changer-api.id}"
-    stage_name = "test"
+    rest_api_id = aws_api_gateway_rest_api.coin-changer-api.id
+    stage_name =  "test"
 }
 
 output "base_url" {
-    value = "${aws_api_gateway_deployment.coin-changer-api.invoke_url}"
+    value = aws_api_gateway_deployment.coin-changer-api.invoke_url
 }
