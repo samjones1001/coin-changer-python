871e63aadff7d858e8ece31a14fc7002a4d6d70c
Sam Jones <sam.jones494@googlemail.com>
2019-09-23 16:26:01 +0100

    add types

--- coinchanger/changer.py
+++ coinchanger/changer.py
@@ -1,4 +1,7 @@
-def calculate(change_amount, coins=None):
+from typing import List
+
+
+def calculate(change_amount: int, coins=None) -> List[int]:
     if coins is None: coins = []
     for coin in (200, 100, 50, 20, 10, 5, 2, 1):
         if coin <= change_amount:
